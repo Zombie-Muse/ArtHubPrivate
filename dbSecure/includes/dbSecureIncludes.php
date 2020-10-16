e49a11371bafa739289759678ab750289b91adbf
Zombie-Muse <gjmccormack@cpp.edu>
2020-10-15 20:31:24 -0700

    fixed file names

--- dbSecure/includes/dbSecureIncludes.php
+++ dbSecure/includes/dbSecureIncludes.php
@@ -5,7 +5,7 @@
 // @version 180426
 
 // Includes common to other cis 4270 applications
-include_once(NON_WEB_BASE_DIR . 'common/includes/cis4270CommonIncludes.php');
+include_once(NON_WEB_BASE_DIR . 'common/includes/artHubCommonIncludes.php');
 
 // Includes specific to this application.
 include_once(APP_NON_WEB_BASE_DIR .'includes/deployment.php');
